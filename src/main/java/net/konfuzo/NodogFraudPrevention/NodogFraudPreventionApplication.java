5cfdf10106c0c8b13c1dc6ec1cdb3d7442776ce6
Anderson Mills <nodog@konfuzo.net>
2023-10-10 22:54:27 -0500

    Add Spring Web from initializr

---
+++ src/main/java/net/konfuzo/NodogFraudPrevention/NodogFraudPreventionApplication.java
@@ -0,0 +1,13 @@
+package net.konfuzo.NodogFraudPrevention;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodogFraudPreventionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NodogFraudPreventionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/konfuzo/NodogFraudPrevention/NodogFraudPreventionApplicationTests.java
@@ -0,0 +1,13 @@
+package net.konfuzo.NodogFraudPrevention;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodogFraudPreventionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
