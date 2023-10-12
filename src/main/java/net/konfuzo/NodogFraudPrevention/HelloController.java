0b854cc3760a21ef60f75de0b535d62b765042db
Anderson Mills <nodog@konfuzo.net>
2023-10-11 22:27:22 -0500

    Add first controller

---
+++ src/main/java/net/konfuzo/NodogFraudPrevention/HelloController.java
@@ -0,0 +1,12 @@
+package net.konfuzo.NodogFraudPrevention;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello from NodogFraudPrevention";
+    }
+}
